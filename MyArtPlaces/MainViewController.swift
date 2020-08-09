a44f139ed0309f5a51193569645c454c1db5eeb5
KazakovaNet <kazakova.net@yandex.ru>
2020-08-10 02:15:01 +0300

    Create UITableViewCOntroller

---
+++ MyArtPlaces/MainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  MyArtPlaces
+//
+//  Created by Natalia Kazakova on 10.08.2020.
+//  Copyright © 2020 Natalia Kazakova. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    //    MARK - UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = "Cell"
+        return cell!
+    }
+}
+
